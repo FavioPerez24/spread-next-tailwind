85be9c760a42eeee9eabb4c9953cdbd57847fec5
Favio Perez <favioperezgarcia@ou.edu>
2025-09-13 21:15:46 -0500

    Initial commit: Next.js + Tailwind setup

---
+++ app/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-red-500 text-5xl font-bold">Hello Tailwind!</h1>
+    </div>
+  )
+}
